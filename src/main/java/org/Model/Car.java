14538ec03b21c9182eb520370457c89ea2fc7768
Mst25947 <144690575+Mst25947@users.noreply.github.com>
2024-06-04 11:26:56 +0200

    Aanpassingen gemaakt aan de Car klasse, categorie klasse toegevoegd

--- src/main/java/org/Model/Car.java
+++ src/main/java/org/Model/Car.java
@@ -3,16 +3,21 @@ import java.util.Date;
 
 public class Car {
     private String brand;
-
     private String type;
-    private Date productionYear;
     private String color;
+    private boolean frontBumper;
+    private boolean rearBumper;
+    private boolean sideSkirt;
+    private boolean spoiler;
 
-    public Car(String brand, String type, Date productionYear, String color){
+    public Car(String brand, String type, String color, boolean frontBumper, boolean rearBumper, boolean sideSkirt, boolean spoiler){
         this.brand = brand;
         this.type = type;
-        this.productionYear = productionYear;
         this.color = color;
+        this.frontBumper = frontBumper;
+        this.rearBumper = rearBumper;
+        this.sideSkirt = sideSkirt;
+        this.spoiler = spoiler;
     }
 }
 
---
+++ src/main/java/org/Model/Catagories.java
@@ -0,0 +1,8 @@
+package org.Model;
+
+public class Catagories {
+    private String frontBumper;
+    private String rearBumper;
+    private String sideSkirt;
+    private String spoiler;
+}
