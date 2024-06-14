b59bf1f5a03779961e3fc6c0d29817b61bb28b7a
Mst25947 <144690575+Mst25947@users.noreply.github.com>
2024-06-14 15:12:13 +0200

    HTML bestanden + references veranderd naar lowercase

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,14 @@ export default defineConfig({
         sourcemap: true,
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'Index.html'),
-                selectCar: resolve(__dirname, './SelectCar.html'),
-                toyota: resolve(__dirname, './Toyota.html'),
-                celicaST20: resolve(__dirname, './Celica-ST20.html'),
-                contact: resolve(__dirname, './Contact.html'),
-                catagories: resolve(__dirname, './Catagories.html'),
-                aboutUs: resolve(__dirname, './About-us.html'),
-                account: resolve(__dirname, './Account.html')
+                main: resolve(__dirname, 'index.html'),
+                selectCar: resolve(__dirname, './selectCar.html'),
+                toyota: resolve(__dirname, './toyota.html'),
+                celicaST20: resolve(__dirname, './celica-ST20.html'),
+                contact: resolve(__dirname, './contact.html'),
+                catagories: resolve(__dirname, './catagories.html'),
+                aboutUs: resolve(__dirname, './about-us.html'),
+                account: resolve(__dirname, './account.html')
 
 // to add more html pages....see FEP1 boilerplate section build&deploy
         },
